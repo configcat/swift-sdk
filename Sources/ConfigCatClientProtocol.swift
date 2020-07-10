548869a470e4f442cbc4707f5323c1ec2d85049c
kp-cat <52385411+kp-cat@users.noreply.github.com>
2020-07-11 00:22:22 +0200

    coverage test (#4)
    * coverage test
    
    * Revert "coverage test"
    
    This reverts commit 957024b50385adde369b836f5cc6562da884645b.
    
    * KeyValue class
    
    * coverage
    
    * coverage
    
    * xcode11
    
    * xcode10
    
    * Coverage update
    
    * Update ConfigCat Coverage.xcscheme
    
    Co-authored-by: Peter Csajtai <peter.csajtai@outlook.com>

--- Sources/ConfigCatClientProtocol.swift
+++ Sources/ConfigCatClientProtocol.swift
@@ -1,14 +1,5 @@
 import Foundation
 
-public class KeyValue : NSObject {
-    let key: String?
-    let value: Any?
-    init(key: String?, value: Any?) {
-        self.key = key
-        self.value = value
-    }
-}
-
 /// Defines the public protocol of the `ConfigCatClient`.
 public protocol ConfigCatClientProtocol {
 
