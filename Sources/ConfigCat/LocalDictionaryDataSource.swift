58f00a4e8d8bc3157bc44f9f9aa75f4519fcc30c
Peter Csajtai <peter.csajtai@outlook.com>
2022-10-19 02:41:34 +0200

    Update LocalDictionaryDataSource.swift

--- Sources/ConfigCat/LocalDictionaryDataSource.swift
+++ Sources/ConfigCat/LocalDictionaryDataSource.swift
@@ -3,7 +3,7 @@ import Foundation
 public class LocalDictionaryDataSource: OverrideDataSource {
     private var settings: [String: Setting] = [:]
 
-    init(source: [String: Any], behaviour: OverrideBehaviour) {
+    @objc public init(source: [String: Any], behaviour: OverrideBehaviour) {
         super.init(behaviour: behaviour)
         for (key, value) in source {
             settings[key] = Setting(value: value, variationId: "", percentageItems: [], rolloutRules: [])
