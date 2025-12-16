32485ba61b66fbf6cf86bccfbd348f0696e7682a
Peter Csajtai <peter.csajtai@outlook.com>
2025-12-16 11:20:10 +0100

    Fix iOS sample (#57)

--- samples/ios/configcatsample/ViewController.swift
+++ samples/ios/configcatsample/ViewController.swift
@@ -2,39 +2,29 @@ import UIKit
 import ConfigCat
 
 class ViewController: UIViewController {
-    
-    var client: ConfigCatClient?
+    let client: ConfigCatClient = ConfigCatClient.get(
+        sdkKey: "PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ") { options in
+
+        // Info level logging helps to inspect the feature flag evaluation process.
+        // Remove this line to avoid too detailed logging in your application.
+        options.logLevel = .info
+
+        // Creating a user object to identify your user (optional).
+        options.defaultUser = ConfigCatUser(identifier: "user-id", email: "configcat@example.com")
+    }
     @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.client = ConfigCatClient.get(
-            sdkKey: "PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A") { options in
-            options.pollingMode = PollingModes.autoPoll(autoPollIntervalInSeconds: 5)
-            options.hooks.addOnConfigChanged { _, _ in
-                self.configChanged()
-            }
-
-            // Info level logging helps to inspect the feature flag evaluation process.
-            // Remove this line to avoid too detailed logging in your application.
-            options.logLevel = .info
-
-            // Creating a user object to identify your user (optional).
-            options.defaultUser = ConfigCatUser(identifier: "user-id", email: "configcat@example.com")
+        Task {
+            let result = await self.client.getValue(for: "isPOCFeatureEnabled", defaultValue: false)
+            label.text = result.description
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func configChanged() {
-        self.client?.getValue(for: "string25Cat25Dog25Falcon25Horse", defaultValue: "") { value in
-            DispatchQueue.main.sync {
-                self.label.text = value
-            }
-        }
     }
 }
